c8f22de39dac6e61cb1738f4fa22a2916f6aaa5b
Dao Zheng <daoyzheng@gmail.com>
2023-02-23 23:08:26 -0700

    Add birthday wish page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom"
 import Admin from "./modules/admin/Admin"
 import AdminLogin from "./modules/admin/AdminLogin"
+import BirthdayWish from "./modules/birthdayWish/BirthdayWish"
 import ProtectedRoute from "./modules/common/ProtectedRoute"
 import Home from "./modules/home/Home"
 import useAuth from "./utils/useAuth"
@@ -11,6 +12,7 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/birthday-wishes" element={<BirthdayWish/>}/>
         <Route path="/admin/login" element={isAuth ? <Navigate to="/admin" /> : <AdminLogin/>}/>
         <Route element={<ProtectedRoute redirectPath="/admin/login"/>}>
           <Route path="/admin" element={isAuth ? <Admin/> : <Navigate to="/admin/login"/>}/>
---
+++ src/modules/birthdayWish/BirthdayWish.tsx
@@ -0,0 +1,11 @@
+import { Page } from "./birthdayWish.styled"
+
+const BirthdayWish = () => {
+  return (
+    <Page className="min-h-screen min-w-screen flex justify-center">
+      HELLO
+    </Page>
+  )
+}
+
+export default BirthdayWish
