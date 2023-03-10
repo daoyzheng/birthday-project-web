3f8731ee7c4a6eea999efd42d7fa74ed20e0b76d
Dao Zheng <daoyzheng@gmail.com>
2023-03-10 16:33:26 -0700

    test loading

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -103,13 +103,13 @@ const Home = () => {
   return !isBirthday ? 
     (
       <>
-      <div className={`bg-stone-900 min-h-screen flex items-center justify-center ${isInitializing ? 'opacity-100' : 'opacity-0 hidden'}`}>
+      <div className={`bg-stone-900 min-h-screen flex items-center justify-center absolute inset-0 ${isInitializing ? 'opacity-100' : 'opacity-0'}`}>
         <div className="fill-white">
           <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><rect x="1" y="1" rx="1" width="10" height="10"><animate id="spinner_c7A9" begin="0;spinner_23zP.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze"/><animate id="spinner_Acnw" begin="spinner_ZmWi.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze"/><animate id="spinner_iIcm" begin="spinner_zfQN.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze"/><animate id="spinner_WX4U" begin="spinner_rRAc.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze"/></rect><rect x="1" y="13" rx="1" width="10" height="10"><animate id="spinner_YLx7" begin="spinner_c7A9.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze"/><animate id="spinner_vwnJ" begin="spinner_Acnw.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze"/><animate id="spinner_KQuy" begin="spinner_iIcm.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze"/><animate id="spinner_arKy" begin="spinner_WX4U.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze"/></rect><rect x="13" y="13" rx="1" width="10" height="10"><animate id="spinner_ZmWi" begin="spinner_YLx7.end" attributeName="x" dur="0.2s" values="13;1" fill="freeze"/><animate id="spinner_zfQN" begin="spinner_vwnJ.end" attributeName="y" dur="0.2s" values="13;1" fill="freeze"/><animate id="spinner_rRAc" begin="spinner_KQuy.end" attributeName="x" dur="0.2s" values="1;13" fill="freeze"/><animate id="spinner_23zP" begin="spinner_arKy.end" attributeName="y" dur="0.2s" values="1;13" fill="freeze"/></rect></svg>
         </div>
         <div className="ml-10 text-lg">{initText}</div>
       </div>
-      <div className={`${isInitializing ? 'opacity-0 hidden' : 'opacity-100'}`}>
+      <div className={`${isInitializing ? 'opacity-0' : 'opacity-100'}`}>
         <Frame>
           <Canvas>
             <Meteor/>
